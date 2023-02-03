ea78ee25189d51978ff6adc3f269a5dd94ea62b9
sanket <sanket.de333@gmail.com>
2023-02-03 22:45:14 +0530

    Organizing Containers of Balls

---
+++ OrganizingContainersOfBalls.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class OrganizingContainersOfBalls {
+
+    public static String organizingContainers(List<List<Integer>> container) {
+
+        int n = container.size();
+        int[] rows = new int[n];
+        int[] cols = new int[n];
+
+        for(int i=0 ;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                rows[i] += container.get(i).get(j);
+                cols[i] += container.get(j).get(i);
+            }
+        }
+        Arrays.sort(rows);
+        Arrays.sort(cols);
+
+
+        if(Arrays.equals(rows,cols))
+            return "Possible";
+        else
+            return "Impossible";
+
+    }
+
+}
+}
