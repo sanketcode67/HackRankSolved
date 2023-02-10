427f5a3d1ee4909937ec8ab6dd986a21fcfc198e
sanket <sanket.de333@gmail.com>
2023-02-10 22:37:56 +0530

    Breaking the Records

---
+++ BreakingTheRecords.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BreakingTheRecords {
+
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+
+        int min = scores.get(0);
+        int max = scores.get(0);
+        int minCount=0;
+        int maxCount=0;
+
+        for(int i=1;i<scores.size();i++)
+        {
+            int score = scores.get(i);
+
+            if(score < min)
+            {
+                minCount++;
+                min = score;
+            }
+            if( score > max)
+            {
+                maxCount++;
+                max = score;
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+        list.add(maxCount);
+        list.add(minCount);
+
+        return list;
+
+
+    }
+}
