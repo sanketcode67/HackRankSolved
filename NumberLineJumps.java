44a023abf64dd808b76c2ca3af0aef481416e33c
sanket <sanket.de333@gmail.com>
2023-02-11 21:11:26 +0530

    Number Line Jumps

---
+++ NumberLineJumps.java
@@ -0,0 +1,15 @@
+public class NumberLineJumps {
+
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+        // Write your code here
+        if(x1<x2 && v1<v2)
+            return "NO";
+        else{
+            if(v1!=v2 && (x2-x1)%(v2-v1)==0)
+                return "YES";
+            else
+                return "NO";
+        }
+
+    }
+}
