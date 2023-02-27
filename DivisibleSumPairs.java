05bd205469f3e6d920688d878b8e6b157cb78abd
sanket <sanket.de333@gmail.com>
2023-02-27 22:50:10 +0530

    Divisible Sum Pairs

---
+++ DivisibleSumPairs.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class DivisibleSumPairs {
+
+    public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
+        int count=0;
+        for(int i=0;i<ar.size()-1;i++)
+        {
+            for(int j=i+1;j<ar.size();j++)
+            {
+                int sum = ar.get(i) + ar.get(j);
+                if(sum % k ==0)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
