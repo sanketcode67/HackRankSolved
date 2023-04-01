7b85bf6620a8a398330f842d4986f88a7bb194c0
sanket <sanket.de333@gmail.com>
2023-04-01 13:12:55 +0530

    Big Sorting

---
+++ BigSorting.java
@@ -0,0 +1,26 @@
+import java.math.BigInteger;
+import java.util.*;
+public class BigSorting {
+    public static List<String> bigSorting(List<String> unsorted) {
+        // Write your code here
+        Collections.sort(unsorted, new Comparator<String>(){
+
+            public int compare(String a,String b)
+            {
+                if(a.length() > b.length()) return 1;
+                else if (a.length() < b.length()) return -1;
+                else
+                {
+                    BigInteger b1 = new BigInteger(a);
+                    BigInteger b2 = new BigInteger(b);
+
+                    return b1.compareTo(b2);
+
+                }
+            }
+
+        });
+        return unsorted;
+
+    }
+}
