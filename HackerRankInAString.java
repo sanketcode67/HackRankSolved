25725d6a42169d1ae7b45464f7ffff18d7f92aa4
sanket <sanket.de333@gmail.com>
2023-04-21 23:56:54 +0530

    HackerRank in a String!

--- HackerRankInAString.java
+++ HackerRankInAString.java
@@ -2,6 +2,7 @@ public class HackerRankInAString {
     public static String hackerrankInString(String s) {
         String str = "hackerrank";
         int n = str.length();
+
         int i=0;
 
         for(char ch : s.toCharArray())
